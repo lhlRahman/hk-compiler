f21c8ea5d96bf19a61ea618bb0c3fd2b8bcc2347
Abdul <lhlrahman@gmail.com>
2024-06-03 22:35:55 -0400

    Add CMakeLists.txt

---
+++ bulid/CMakeFiles/HK.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HK.dir/src/main.cpp.o"
+  "CMakeFiles/HK.dir/src/main.cpp.o.d"
+  "HK"
+  "HK.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
